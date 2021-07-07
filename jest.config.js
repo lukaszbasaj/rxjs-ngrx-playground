e1b3b4348993157810eb353bc92bfbb3a0cbda1d
Łukasz Basaj <lbasaj@gmail.com>
2021-07-07 14:22:20 +0200

    feat: initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/rxjs-ngrx-playground', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/rxjs-ngrx-playground',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/products',
+  ],
 };
